2517559b8c855fd89f4c1fcc78519c100881d501
paulossjunior <paulossjunior@gmail.com>
2015-12-05 16:09:31 -0200

    Adicionando Login e comentários

---
+++ django_bootstrap_extension/blog/migrations/0002_comment.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('author', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
+                ('approved_comment', models.BooleanField(default=False)),
+                ('post', models.ForeignKey(to='blog.Post', related_name='comments')),
+            ],
+        ),
+    ]
